2ac5dbec00ca746046a35ff7db5476c527562399
BuildTools <unconfigured@null.spigotmc.org>
2022-10-21 11:05:31 +0000

    Some cleanup

--- mc/src/main/java/org/cubeville/portal/BQWrapper.java
+++ mc/src/main/java/org/cubeville/portal/BQWrapper.java
@@ -10,39 +10,36 @@ class BQWrapper
 {
     private Object betonQuest;
     private boolean bqNewVersion;
-    
+
     private Method bqConditionMethod;
     private Method bqGetIdMethod;
     private Class<?> bqConditionIdClass;
-    private Constructor<?> conditionIdCtor;
-    private Class<?> playerConverter;
-    private Class<?> bqClass;
-    
+    private Constructor<?> bqConditionIdCtor;
+
     BQWrapper(PluginManager pm) {
         betonQuest = pm.getPlugin("BetonQuest");
         if(betonQuest == null) return;
 
         try {
             bqConditionIdClass = Class.forName("org.betonquest.betonquest.id.ConditionID");
-            bqClass = Class.forName("org.betonquest.betonquest.BetonQuest");
         }
         catch (ClassNotFoundException e) {
             betonQuest = null;
             return;
         }
-        
+
         bqNewVersion = false;
         try {
             bqConditionMethod = betonQuest.getClass().getMethod("condition", Class.forName("org.betonquest.betonquest.api.profiles.Profile"), Class.forName("org.betonquest.betonquest.id.ConditionID"));
-            playerConverter = Class.forName("org.betonquest.betonquest.utils.PlayerConverter");
-            bqGetIdMethod = playerConverter.getMethod("getID", Player.class);            
+            Class<?> playerConverter = Class.forName("org.betonquest.betonquest.utils.PlayerConverter");
+            bqGetIdMethod = playerConverter.getMethod("getID", Player.class);
+            bqConditionIdCtor = bqConditionIdClass.getConstructor(Class.forName("org.betonquest.betonquest.api.config.QuestPackage"), String.class);
             bqNewVersion = true;
-            conditionIdCtor = bqConditionIdClass.getConstructor(Class.forName("org.betonquest.betonquest.api.config.QuestPackage"), String.class);
         }
         catch(Exception e) {
             try {
                 bqConditionMethod = betonQuest.getClass().getMethod("condition", String.class, Class.forName("org.betonquest.betonquest.id.ConditionID"));
-                conditionIdCtor = bqConditionIdClass.getConstructor(Class.forName("org.betonquest.betonquest.config.ConfigPackage"), String.class);
+                bqConditionIdCtor = bqConditionIdClass.getConstructor(Class.forName("org.betonquest.betonquest.config.ConfigPackage"), String.class);
             }
             catch(Exception ex) {
                 betonQuest = null;
@@ -54,24 +51,20 @@ class BQWrapper
         if(betonQuest == null) return false;
 
         try {
-            
             if(bqNewVersion) {
-                Object conditionId = conditionIdCtor.newInstance(null, condition);
-                Object playerId = bqGetIdMethod.invoke(playerConverter, player);
-                return (boolean)bqConditionMethod.invoke(bqClass, playerId, conditionId);                
+                Object conditionId = bqConditionIdCtor.newInstance(null, condition);
+                Object playerId = bqGetIdMethod.invoke(null, player);
+                return (boolean)bqConditionMethod.invoke(null, playerId, conditionId);
             }
-
             else {
-                Object conditionId = conditionIdCtor.newInstance(null, condition);
-                return (boolean)bqConditionMethod.invoke(bqClass, player.getUniqueId().toString(), conditionId);                
+                Object conditionId = bqConditionIdCtor.newInstance(null, condition);
+                return (boolean)bqConditionMethod.invoke(null, player.getUniqueId().toString(), conditionId);
             }
-            
         }
         catch (Exception e) {
             System.out.println(e);
             return false;
         }
     }
-
 
 }
